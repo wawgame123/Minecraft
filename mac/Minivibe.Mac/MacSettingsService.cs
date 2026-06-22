e07787ac598700b09b4552f57bec27c16c125f21
wawgame123 <156342924+wawgame123@users.noreply.github.com>
2026-06-22 17:37:06 +0300

    Release minivibe 0.4.1

--- mac/Minivibe.Mac/MacSettingsService.cs
+++ mac/Minivibe.Mac/MacSettingsService.cs
@@ -30,7 +30,7 @@ internal sealed class MacSettingsService
                 SkinServerUrl = LauncherSettings.DefaultSkinServerUrl,
                 EnableSkinServer = true,
                 EnableAutoUpdate = true,
-                LastSeenLauncherVersion = "0.4.0"
+                LastSeenLauncherVersion = "0.4.1"
             };
             await SaveAsync(settings);
             return settings;
